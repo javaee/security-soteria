a4949c6fdfc8145e7cd8605f24b5d9f684116bd8
arjan tijms <arjan.tijms@gmail.com>
2017-06-28 18:47:50 +0200

    #68 Impl responseUnAuthorized to responseUnauthorized

--- test/app-custom-rememberme/src/main/java/org/glassfish/soteria/test/TestAuthenticationMechanism.java
+++ test/app-custom-rememberme/src/main/java/org/glassfish/soteria/test/TestAuthenticationMechanism.java
@@ -93,7 +93,7 @@ public class TestAuthenticationMechanism implements HttpAuthenticationMechanism 
                 return httpMessageContext.notifyContainerAboutLogin(
                     result.getCallerPrincipal(), result.getCallerGroups());
             } else {
-                return httpMessageContext.responseUnAuthorized();
+                return httpMessageContext.responseUnauthorized();
             }
         } 
 
