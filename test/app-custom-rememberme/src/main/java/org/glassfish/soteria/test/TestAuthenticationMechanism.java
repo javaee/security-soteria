e6898335529b6aa32947f29349755b384bee0ebf
arjan tijms <arjan.tijms@gmail.com>
2017-07-03 00:28:09 +0200

    #63 implement httpOnly setting for remember me and added test

--- test/app-custom-rememberme/src/main/java/org/glassfish/soteria/test/TestAuthenticationMechanism.java
+++ test/app-custom-rememberme/src/main/java/org/glassfish/soteria/test/TestAuthenticationMechanism.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2015, 2016 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2017 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -104,10 +104,10 @@ public class TestAuthenticationMechanism implements HttpAuthenticationMechanism 
         return httpMessageContext.getRequest().getParameter("rememberme") != null;
     }
     
-    // Workaround for possible CDI bug; at least in Weld 2.3.2 default methods don't seem to be intercepted
+    // Workaround for Weld bug; at least in Weld 2.3.2 default methods are not intercepted
     @Override
     public void cleanSubject(HttpServletRequest request, HttpServletResponse response, HttpMessageContext httpMessageContext) {
-    	HttpAuthenticationMechanism.super.cleanSubject(request, response, httpMessageContext);
+    	    HttpAuthenticationMechanism.super.cleanSubject(request, response, httpMessageContext);
     }
     
 }
